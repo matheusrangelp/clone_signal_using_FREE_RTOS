65168c9a33471766c9e08adfbb7b65f5d176c7df
Matheus Pimentel <matheusrpimentel@gmail.com>
2022-03-15 21:28:57 -0300

    update

---
+++ tv2/gcc/timers.d
@@ -0,0 +1,24 @@
+gcc/timers.o: C:\FreeRTOS-LTS/FreeRTOS/Source/timers.c \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/FreeRTOS.h FreeRTOSConfig.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/projdefs.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/portable.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/deprecated_definitions.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/portable/GCC/ARM_CM3/portmacro.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/task.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/list.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/queue.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/task.h \
+ C:\FreeRTOS-LTS/FreeRTOS/Source/include/timers.h
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/FreeRTOS.h:
+FreeRTOSConfig.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/projdefs.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/portable.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/deprecated_definitions.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/portable/GCC/ARM_CM3/portmacro.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/mpu_wrappers.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/task.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/list.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/queue.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/task.h:
+C:\FreeRTOS-LTS/FreeRTOS/Source/include/timers.h:
